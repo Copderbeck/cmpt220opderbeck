0c6036bf41d0625c2a8208f4e517a2c741e1ca1d
Copderbeck <connor.opderbeck1@marist.edu>
2017-03-29 16:45:24 -0400

    Lab 8

---
+++ labs/8/Lab8/src/SavingsAccount.java
@@ -0,0 +1,16 @@
+public class SavingsAccount
+		extends Account {
+	public SavingsAccount() {
+		super();
+	}
+	public SavingsAccount(int id, double balance) {
+		super(id, balance);
+	}
+	public void withdraw(double amount) {
+		if (amount < getBalance()) {
+			setBalance(getBalance() - amount);
+		}
+		else
+			System.out.println(" Savings account overdrawn transtaction rejected");
+	}
+}
